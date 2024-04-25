f6426feb3737fca0f82e4ab278e152744ca5c8e1
Lasha <robaqidzelasha@yahoo.com>
2024-04-26 00:21:16 +0400

    init

---
+++ script.js
@@ -0,0 +1,186 @@
+
+
+
+let imgs = [
+    'https://randomwordgenerator.com/img/picture-generator/50e6d1414c50b10ff3d8992cc12c30771037dbf85254784e77267ed09444_640.jpg',
+    'https://randomwordgenerator.com/img/picture-generator/50e6d1414c50b10ff3d8992cc12c30771037dbf85254784e77267ed09444_640.jpg',
+    'https://randomwordgenerator.com/img/picture-generator/gefb09637a3a21820315fba42a115b27d2bcf4079ed3a4538445490993df71524a886dcea8021c5d9949b42108dcc2932_640.jpg',
+    'https://randomwordgenerator.com/img/picture-generator/gefb09637a3a21820315fba42a115b27d2bcf4079ed3a4538445490993df71524a886dcea8021c5d9949b42108dcc2932_640.jpg',
+    'https://randomwordgenerator.com/img/picture-generator/55e5d5434b55ad14f1dc8460962e33791c3ad6e04e507440762a7cd69449c4_640.jpg',
+    'https://randomwordgenerator.com/img/picture-generator/55e5d5434b55ad14f1dc8460962e33791c3ad6e04e507440762a7cd69449c4_640.jpg',
+    'https://randomwordgenerator.com/img/picture-generator/52e7dd424c5aa514f1dc8460962e33791c3ad6e04e5074417d2e7ed6964acd_640.jpg',
+    'https://randomwordgenerator.com/img/picture-generator/52e7dd424c5aa514f1dc8460962e33791c3ad6e04e5074417d2e7ed6964acd_640.jpg',
+    'https://randomwordgenerator.com/img/picture-generator/55e8d4414b54b10ff3d8992cc12c30771037dbf85254784e77267fd2954e_640.jpg',
+    'https://randomwordgenerator.com/img/picture-generator/55e8d4414b54b10ff3d8992cc12c30771037dbf85254784e77267fd2954e_640.jpg',
+    'https://randomwordgenerator.com/img/picture-generator/david-kovalenko-G85VuTpw6jg-unsplash.jpg',
+    'https://randomwordgenerator.com/img/picture-generator/david-kovalenko-G85VuTpw6jg-unsplash.jpg',
+    'https://randomwordgenerator.com/img/picture-generator/stones-3364325_640.jpg',
+    'https://randomwordgenerator.com/img/picture-generator/stones-3364325_640.jpg',
+    // 'https://randomwordgenerator.com/img/picture-generator/53e0d5424b57b10ff3d8992cc12c30771037dbf85254794e732a7ddd9645_640.jpg',
+    //     'https://randomwordgenerator.com/img/picture-generator/53e0d5424b57b10ff3d8992cc12c30771037dbf85254794e732a7ddd9645_640.jpg',
+    // 'https://randomwordgenerator.com/img/picture-generator/5fe9d3444856b10ff3d8992cc12c30771037dbf852547849722f7bd6924e_640.jpg',
+    //     'https://randomwordgenerator.com/img/picture-generator/5fe9d3444856b10ff3d8992cc12c30771037dbf852547849722f7bd6924e_640.jpg',
+    // 'https://randomwordgenerator.com/img/picture-generator/52e3d6434253ad14f1dc8460962e33791c3ad6e04e507440742a7ad6954cc4_640.jpg',
+    //     'https://randomwordgenerator.com/img/picture-generator/52e3d6434253ad14f1dc8460962e33791c3ad6e04e507440742a7ad6954cc4_640.jpg',
+    // 'https://randomwordgenerator.com/img/picture-generator/57e7d74a4b57ab14f1dc8460962e33791c3ad6e04e507441722973d49645c2_640.jpg',
+    //     'https://randomwordgenerator.com/img/picture-generator/57e7d74a4b57ab14f1dc8460962e33791c3ad6e04e507441722973d49645c2_640.jpg',
+    'https://randomwordgenerator.com/img/picture-generator/57e0d6424b51ad14f1dc8460962e33791c3ad6e04e50744172297bd6954fc0_640.jpg',
+    'https://randomwordgenerator.com/img/picture-generator/57e0d6424b51ad14f1dc8460962e33791c3ad6e04e50744172297bd6954fc0_640.jpg'
+]
+
+
+
+let imgss = [
+    'https://randomwordgenerator.com/img/picture-generator/50e6d1414c50b10ff3d8992cc12c30771037dbf85254784e77267ed09444_640.jpg',
+    'https://randomwordgenerator.com/img/picture-generator/50e6d1414c50b10ff3d8992cc12c30771037dbf85254784e77267ed09444_640.jpg',
+    'https://randomwordgenerator.com/img/picture-generator/gefb09637a3a21820315fba42a115b27d2bcf4079ed3a4538445490993df71524a886dcea8021c5d9949b42108dcc2932_640.jpg',
+    'https://randomwordgenerator.com/img/picture-generator/gefb09637a3a21820315fba42a115b27d2bcf4079ed3a4538445490993df71524a886dcea8021c5d9949b42108dcc2932_640.jpg',
+    'https://randomwordgenerator.com/img/picture-generator/55e5d5434b55ad14f1dc8460962e33791c3ad6e04e507440762a7cd69449c4_640.jpg',
+    'https://randomwordgenerator.com/img/picture-generator/55e5d5434b55ad14f1dc8460962e33791c3ad6e04e507440762a7cd69449c4_640.jpg',
+    'https://randomwordgenerator.com/img/picture-generator/52e7dd424c5aa514f1dc8460962e33791c3ad6e04e5074417d2e7ed6964acd_640.jpg',
+    'https://randomwordgenerator.com/img/picture-generator/52e7dd424c5aa514f1dc8460962e33791c3ad6e04e5074417d2e7ed6964acd_640.jpg',
+    'https://randomwordgenerator.com/img/picture-generator/55e8d4414b54b10ff3d8992cc12c30771037dbf85254784e77267fd2954e_640.jpg',
+    'https://randomwordgenerator.com/img/picture-generator/55e8d4414b54b10ff3d8992cc12c30771037dbf85254784e77267fd2954e_640.jpg',
+    'https://randomwordgenerator.com/img/picture-generator/david-kovalenko-G85VuTpw6jg-unsplash.jpg',
+    'https://randomwordgenerator.com/img/picture-generator/david-kovalenko-G85VuTpw6jg-unsplash.jpg',
+    'https://randomwordgenerator.com/img/picture-generator/stones-3364325_640.jpg',
+    'https://randomwordgenerator.com/img/picture-generator/stones-3364325_640.jpg',
+    'https://randomwordgenerator.com/img/picture-generator/53e0d5424b57b10ff3d8992cc12c30771037dbf85254794e732a7ddd9645_640.jpg',
+    'https://randomwordgenerator.com/img/picture-generator/53e0d5424b57b10ff3d8992cc12c30771037dbf85254794e732a7ddd9645_640.jpg',
+    'https://randomwordgenerator.com/img/picture-generator/5fe9d3444856b10ff3d8992cc12c30771037dbf852547849722f7bd6924e_640.jpg',
+    'https://randomwordgenerator.com/img/picture-generator/5fe9d3444856b10ff3d8992cc12c30771037dbf852547849722f7bd6924e_640.jpg',
+    'https://randomwordgenerator.com/img/picture-generator/52e3d6434253ad14f1dc8460962e33791c3ad6e04e507440742a7ad6954cc4_640.jpg',
+    'https://randomwordgenerator.com/img/picture-generator/52e3d6434253ad14f1dc8460962e33791c3ad6e04e507440742a7ad6954cc4_640.jpg',
+    'https://randomwordgenerator.com/img/picture-generator/57e7d74a4b57ab14f1dc8460962e33791c3ad6e04e507441722973d49645c2_640.jpg',
+    'https://randomwordgenerator.com/img/picture-generator/57e7d74a4b57ab14f1dc8460962e33791c3ad6e04e507441722973d49645c2_640.jpg',
+    'https://randomwordgenerator.com/img/picture-generator/57e0d6424b51ad14f1dc8460962e33791c3ad6e04e50744172297bd6954fc0_640.jpg',
+    'https://randomwordgenerator.com/img/picture-generator/57e0d6424b51ad14f1dc8460962e33791c3ad6e04e50744172297bd6954fc0_640.jpg'
+]
+
+
+
+
+let game = document.querySelector('.game')
+
+let choosCard = document.querySelectorAll('.choosCard')
+
+let reset = document.querySelector('.reset')
+reset.style.display = 'none';
+
+let rand;
+
+let n = 0;
+
+
+
+function myFunction(x) {
+
+    reset.style.display = 'block';
+
+    for (let i = 0; i < x; i++) {
+
+        let card = document.createElement('div');
+
+        if (x == 16) {
+
+
+            rand = Math.floor(Math.random() * imgs.length);
+
+            card.innerHTML = `<img src="${imgs[rand]}" alt="">`;
+
+            imgs.splice(rand, 1);
+
+        }
+
+        else if (x == 24) {
+            rand = Math.floor(Math.random() * imgss.length);
+
+            card.innerHTML = `<img src="${imgss[rand]}" alt="">`;
+
+            imgss.splice(rand, 1);
+        }
+
+
+
+        card.onclick = function () {
+            this.classList.add('openCard')
+
+            setTimeout(function () {
+
+                if (document.querySelectorAll('.openCard').length > 1) {
+
+                    
+                    n += 1;
+                    
+
+                    if (document.querySelectorAll('.openCard')[0].querySelector('img').src
+                        == document.querySelectorAll('.openCard')[1].querySelector('img').src) {
+
+
+
+                        document.querySelectorAll('.openCard')[0].classList.add('same')
+                        document.querySelectorAll('.openCard')[1].classList.add('same')
+
+                        document.querySelectorAll('.openCard')[1].classList.remove('openCard')
+                        document.querySelectorAll('.openCard')[0].classList.remove('openCard')
+
+
+                        localStorage.setItem('attemp', n)
+
+                        if (document.querySelectorAll('.same').length == x) {
+                            alert('Complete the game in' + ' '+ localStorage.getItem('attemp') +' '+'try')
+                        }
+
+
+
+                    }
+                    else {
+                        document.querySelectorAll('.openCard')[1].classList.remove('openCard')
+
+                        document.querySelectorAll('.openCard')[0].classList.remove('openCard')
+
+                    }
+                    
+                    
+
+
+                }
+
+                
+
+
+            }, 600)
+
+            
+
+
+        }
+
+
+        card.classList.add('card');
+
+        
+
+   
+        game.appendChild(card);
+
+
+    }
+
+    let buttons = document.getElementsByClassName('choosCard');
+
+
+    Array.from(buttons).forEach(element => {
+        element.classList.add('hidden')
+    });
+
+
+
+}
+
+
+
+
+
+
+
